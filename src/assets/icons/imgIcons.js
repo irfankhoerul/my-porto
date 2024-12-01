04c98d07300b88d2bd8cc8f35673f858448ac14d
irfankhoerul <irfankhoerul12@gmail.com>
2024-12-01 23:20:02 +0700

    Init : Firts Init Project Unfinished

---
+++ src/assets/icons/imgIcons.js
@@ -0,0 +1,4 @@
+import webIcon from './web-development_2210153.png';
+import designIcon from './designer_2091370.png';
+
+export { webIcon, designIcon };
---
+++ src/assets/icons_skill/imgAssets.js
@@ -0,0 +1,68 @@
+import figmaIcon from "./figma-v2.png";
+import canvaIcon from "./canva-v2.png";
+import blenderIcon from "./blender-v2.png";
+import adobePremiereIcon from "./adobe-premire-v2.png";
+import reactIcon from "./react-v2.png";
+import laravelIcon from "./laravel-v2.png";
+import springBootIcon from "./spring-boot-v2.png";
+import pythonIcon from "./python-v2.png";
+import javascriptIcon from "./javascript-v2.png";
+import tailwindCssIcon from "./tailwind-v2.png";
+import mysqlIcon from "./mysql-v2.png";
+import postgresqlIcon from "./postgres-v2.png";
+import gitIcon from "./Git-Icon-1788C.png";
+
+export const skills = [
+  {
+    name: "Figma",
+    src: figmaIcon,
+  },
+  {
+    name: "Canva",
+    src: canvaIcon,
+  },
+  {
+    name: "Blender",
+    src: blenderIcon,
+  },
+  {
+    name: "Adobe Premiere",
+    src: adobePremiereIcon,
+  },
+  {
+    name: "React",
+    src: reactIcon,
+  },
+  {
+    name: "Laravel",
+    src: laravelIcon,
+  },
+  {
+    name: "Spring Boot",
+    src: springBootIcon,
+  },
+  {
+    name: "Python",
+    src: pythonIcon,
+  },
+  {
+    name: "JavaScript",
+    src: javascriptIcon,
+  },
+  {
+    name: "Tailwind CSS",
+    src: tailwindCssIcon,
+  },
+  {
+    name: "MySQL",
+    src: mysqlIcon,
+  },
+  {
+    name: "PostgreSQL",
+    src: postgresqlIcon,
+  },
+  {
+    name: "Git",
+    src: gitIcon,
+  },
+];
